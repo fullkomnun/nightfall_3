fef2c1b8724b62ffa254e8fc826de992cde567b1
Or Noyman <17881579+fullkomnun@users.noreply.github.com>
2023-01-23 21:17:09 +0200

    chore: try docker-build-push action

--- bin/build-publish-circom.sh
+++ bin/build-publish-circom.sh
@@ -11,26 +11,42 @@ export USER="fullkomnun"
 export IMAGE_NAME="local-circom"
 export IMAGE_TAG="latest"
 
-# Create a multi-architecture manifest
-podman manifest create ${MANIFEST_NAME}
+# Make sure to login via docker/podman CLI using your GitHub PAT with 'packages:write' permissions
+# cat ~/.github/token | docker login ghcr.io --username $USER --password-stdin
 
-# Build your amd64 architecture container
-podman build \
-    --no-cache \
-    --tag "${REGISTRY}/${USER}/${IMAGE_NAME}:${IMAGE_TAG}" \
-    --manifest ${MANIFEST_NAME} \
-    --arch amd64 \
-    - < docker/circom.Dockerfile
+if hash podman 2>/dev/null; then
+    echo "using podman"
 
-# Build your arm64 architecture container
-podman build \
-    --no-cache \
-    --tag "${REGISTRY}/${USER}/${IMAGE_NAME}:${IMAGE_TAG}" \
-    --manifest ${MANIFEST_NAME} \
-    --arch arm64 \
-    - < docker/circom.Dockerfile
+    # Create a multi-architecture manifest
+    podman manifest create ${MANIFEST_NAME}
 
-# Push the full manifest, with both CPU Architectures
-podman manifest push --all \
-    ${MANIFEST_NAME} \
-    "docker://${REGISTRY}/${USER}/${IMAGE_NAME}:${IMAGE_TAG}"
+    # Build your amd64 architecture container
+    podman build \
+        --no-cache \
+        --tag "${REGISTRY}/${USER}/${IMAGE_NAME}:${IMAGE_TAG}" \
+        --manifest ${MANIFEST_NAME} \
+        --arch amd64 \
+        - < docker/circom.Dockerfile
+
+    # Build your arm64 architecture container
+    podman build \
+        --no-cache \
+        --tag "${REGISTRY}/${USER}/${IMAGE_NAME}:${IMAGE_TAG}" \
+        --manifest ${MANIFEST_NAME} \
+        --arch arm64 \
+        - < docker/circom.Dockerfile
+
+    # Push the full manifest, with both CPU Architectures
+    podman manifest push --all \
+        ${MANIFEST_NAME} \
+        "docker://${REGISTRY}/${USER}/${IMAGE_NAME}:${IMAGE_TAG}"
+else
+    echo "using docker"
+
+    # Build both amd64 and arm64 architecure containers and push
+    docker buildx build \
+        --no-cache \
+        --platform linux/amd64,linux/arm64 \
+        --tag "${REGISTRY}/${USER}/${IMAGE_NAME}:${IMAGE_TAG}" \
+        --push - < docker/circom.Dockerfile
+fi
